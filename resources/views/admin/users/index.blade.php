c53df7b54a51893c9022514d89b0b23ce91320ce
DhikaRoidHakim <itsro.work@gmail.com>
2025-08-12 17:06:10 +0700

    tes update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -12,7 +12,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
         </svg>
-        <span>{{ session('success') }}</span>
+        <span>{{ session('success') }} Gan</span>
     </div>
 @endif
 <div class="p-6 bg-white rounded-md shadow-sm">
